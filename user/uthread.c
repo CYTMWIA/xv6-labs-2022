568c1508e9b6aaaedf818e2426a6436167b2dd40
CYTMWIA <cytmwia@gmail.com>
2023-05-16 13:10:25 +0800

    uthread

--- user/uthread.c
+++ user/uthread.c
@@ -60,8 +60,8 @@ thread_schedule(void)
     current_thread = next_thread;
     /* YOUR CODE HERE
      * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
      */
+    thread_switch((uint64)(t->stack), (uint64)(current_thread->stack));
   } else
     next_thread = 0;
 }
@@ -76,6 +76,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  *((uint64*)(t->stack) + 0) = (uint64)func;
+  *((uint64*)(t->stack) + 1) = (uint64)&(t->stack[STACK_SIZE]);
 }
 
 void 
